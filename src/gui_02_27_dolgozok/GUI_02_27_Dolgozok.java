101eed4b09c148dbba3e6eb46d532742a20bde3e
PIKSSL <purpledog120@gmail.com>
2023-02-27 13:06:13 +0100

    init

---
+++ src/gui_02_27_dolgozok/GUI_02_27_Dolgozok.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gui_02_27_dolgozok;
+
+/**
+ *
+ * @author gyorgy.krisztian
+ */
+public class GUI_02_27_Dolgozok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
